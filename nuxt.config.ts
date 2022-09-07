ca7909b7ef04aca3965d117429a3ef43932292e6
oceaann <47253870+oceaann@users.noreply.github.com>
2022-09-07 19:10:38 +0200

    (temp, fix): headlessui switch

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import { defineNuxtConfig } from 'nuxt'
 export default defineNuxtConfig({
     css: ["~/assets/css/tailwind.css"],
     build: {
+        transpile: ['@headlessui/vue'],
         postcss: {
             postcssOptions: {
                 plugins: {
