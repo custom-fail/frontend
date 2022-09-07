cf3903a9ff2cc9e03b4c46eb2cc141cad3657a67
oceaann <47253870+oceaann@users.noreply.github.com>
2022-09-07 19:13:19 +0200

    dynamic ref update in switch

--- components/SwitchLR.vue
+++ components/SwitchLR.vue
@@ -1,19 +1,17 @@
 <template>
   <Switch
-      v-model="enabled"
-      :class="enabled ? `bg-[#456E4DFF] left 0.15s ease-out` : `bg-[#555762FF]`"
+      v-model="modelValue" @update:modelValue="$emit(`update:modelValue`, $event)"
+      :class="modelValue ? `bg-[#456E4DFF] left 0.15s ease-out` : `bg-[#555762FF]`"
       class="relative inline-flex items-center h-[28px] rounded-full transition-colors w-[52px] border-white border-[1px]"
   >
-    <span :class="enabled ? `translate-x-[26px]` : `translate-x-[4px]`"
+    <span :class="modelValue ? `translate-x-[26px]` : `translate-x-[4px]`"
           class="inline-block transform h-[20px] w-[20px] rounded-full bg-white transition-transform transform"
     />
   </Switch>
 </template>
 
 <script setup>
-import {ref} from "vue"
 import { Switch } from "@headlessui/vue"
-const { modelValue } = defineProps(["modelValue"])
-const enabled = ref(modelValue)
-
+let { modelValue } = defineProps(["modelValue"])
+defineEmits(["update:modelValue"])
 </script>
