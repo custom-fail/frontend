043f5da25f0dee17ac7d083a56afbc009a39a5d6
oceaann <47253870+oceaann@users.noreply.github.com>
2022-09-18 23:39:37 +0200

    add hotjar tracking

--- app.vue
+++ app.vue
@@ -14,3 +14,18 @@ body {
   display: inherit;
 }
 </style>
+
+<script setup>
+useHead({
+  script: [{
+    children: `(function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:3111264,hjsv:6};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+  }]
+})
+</script>
