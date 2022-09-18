36d5c4889630352404612737b68dbd08479f12af
oceaann <47253870+oceaann@users.noreply.github.com>
2022-09-18 22:13:29 +0200

    moderation section in the main page

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,54 @@
 
     </div>
     <Separator />
-    <div class="pr-[5%] pl-[5%]">
+    <div class="pr-[5%] pl-[5%] bg-[#121313] my-[50px]">
+      <div>
+        <div class="flex justify-between">
+          <div class="flex justify-center place-items-center">
+            <div class="mb-[20px]">
+              <span class="text-3xl font-bold mb-[20px]">
+                Moderation with quality out of the box
+              </span><br>
+              <div class="ml-[5px]">
+                <div class="flex" v-for="text in [`Cases (punishments saving)`, `Auto Moderation`, `Utils commands`]">
+                  <img class="h-[25px]" src="/img/icons8-approved-checkmark-symbol-to-verify-the-result.svg">
+                  {{ text }}
+                </div>
+              </div>
+
+            </div>
+          </div>
+          <img src="/img/previews/AntiDuplicate.svg" class="w-[35%] ml-[5%] mb-[60px]">
+        </div>
+        <div class="flex">
+          <img src="/img/previews/CaseLast.svg" class="w-[35%] mr-[60px]">
+          <img src="/img/previews/AutoMod.svg" class="w-[35%] ml-[60px]">
+          <img src="/img/previews/AutoModLogs.svg" class="w-[35%] ml-[60px]">
+          <img src="/img/previews/Timeout.svg" class="w-[35%] ml-[60px]">
+        </div>
+      </div>
+    </div>
+    <Separator />
+    <div class="flex justify-between mx-[15%] my-[50px]">
+      <div class="flex place-items-center">
+        <span>Ready for using inviting Custom to your server?</span>
+      </div>
+      <div>
+        <NuxtLink href="/servers">
+          <button class="bg-[#dcc7c7] p-[10px] text-black rounded mr-[10px]">Open Dashboard</button>
+        </NuxtLink>
+        <NuxtLink href="/support">
+          <button class="bg-[#3c56a4] p-[10px] text-white rounded mr-[10px]">Documentation</button>
+        </NuxtLink>
+        <NuxtLink href="https://discord.gg/7HUDwwZjGt">
+          <button class="bg-[#3c56a4] p-[10px] text-white rounded">Support Server</button>
+        </NuxtLink>
+      </div>
+    </div>
+    <Separator />
+    <div class="pr-[5%] pl-[5%] my-[50px]">
       <div class="flex justify-center mb-[100px]">
-        <img class="w-[35%] max-w-[500px] mr-[30px]" src="/img/Frame3.svg" />
+        <img class="w-[35%] max-w-[500px] mr-[30px]" src="/img/previews/ContextMenus.svg" />
         <div class="place-items-center grid">
           <div>
             <span class="text-white text-[20px]">Context menus</span><br>
@@ -21,7 +66,7 @@
             <span>We are making everything intuitive. Accessibility is our top priority!</span>
           </div>
         </div>
-        <img class="w-[35%] max-w-[500px] ml-[30px]" src="/img/Frame5.svg" />
+        <img class="w-[35%] max-w-[500px] ml-[30px]" src="/img/previews/Modals.svg" />
       </div>
     </div>
     <Separator />
