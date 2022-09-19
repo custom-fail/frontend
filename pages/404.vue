3d100707048186d343e81367236e54f6bb504d9e
oceaann <47253870+oceaann@users.noreply.github.com>
2022-09-19 23:14:35 +0200

    add 404 page

---
+++ pages/404.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="h-[calc(100vh-80px)] w-full bg-background flex place-items-center justify-center">
+    <span class="text-white text-[90px]">Page not found</span>
+  </div>
+</template>
+
+<script>
+definePageMeta({
+  layout: "none"
+})
+</script>
