bd2a359625e8cb89b5ef47409b27ce59c0d5cd6a
oceaann <47253870+oceaann@users.noreply.github.com>
2022-09-24 13:49:02 +0200

    apply SwapSidebar

--- layouts/server.vue
+++ layouts/server.vue
@@ -1,31 +1,62 @@
 <template>
-  <div class="grid main">
-    <Sidebar />
-    <main>
-      <Navbar />
-      <div class="h-[calc(100vh-80px)] bg-background w-full overflow-y-scroll mt-[80px] text-white">
+  <div class="server-layout">
+    <SwapSidebar
+        v-if="screenWidth < 791" class="z-50" v-model="isSidebarOpen"
+    >
+      <Sidebar />
+    </SwapSidebar>
+    <Sidebar v-else />
+    <section>
+      <nav class="h-[80px] w-full">
+        <ListBullet
+            class="text-white h-full ml-[50px] cursor-pointer sidebar-open-button"
+            @click="isSidebarOpen = !isSidebarOpen"
+        />
+      </nav>
+      <div class="h-[calc(100vh-80px)] bg-background w-full overflow-y-scroll  text-white">
         <div class="m-[20px] flex justify-center page">
           <div class="max-w-[1500px] w-full">
             <slot />
           </div>
         </div>
       </div>
-    </main>
+    </section>
   </div>
 </template>
 
 <script setup>
 import Sidebar from "../components/sidebar"
-import Navbar from "../components/navbar"
+import SwapSidebar from "../components/SwipeSidebar";
+import {onMounted} from "vue";
+
+import { ref } from "vue"
+import ListBullet from "../components/icons/ListBullet";
+
+const isSidebarOpen = ref(false)
+
+const screenWidth = ref(0)
+onMounted(() => {
+  screenWidth.value = window.innerWidth
+  window.addEventListener('resize', () => {
+    screenWidth.value = window.innerWidth
+  })
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', () => {
+    screenWidth.value = window.innerWidth
+  })
+})
 </script>
 
 <style>
-.page {
-  z-index: -1;
+@media only screen and (min-width: 791px) {
+  .server-layout {
+    @apply grid grid-cols-[300px,1fr];
+  }
+  .sidebar-open-button { @apply hidden}
 }
-.main {
-  grid-template-columns: 300px 1fr;
-}
+
 textarea:focus, input:focus {
   outline: none;
 }
